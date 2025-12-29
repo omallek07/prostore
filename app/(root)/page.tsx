5af6932ba9ed55c4cf7a1d4c634ade36fc473f68
Kevin O'Malley <omallek@gmail.com>
2025-12-29 09:37:25 -0500

    Add dummy data, add product card and price components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
+
 const Homepage = () => {
-  return <main>HOME PAGE</main>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,29 @@
+import ProductCard from './product-card';
+
+type ProductListProps = {
+  data: any;
+  title?: string;
+  limit?: number;
+};
+
+function ProductList({ data, title, limit }: ProductListProps) {
+  const limitedData = limit ? data.slice(0, limit) : data;
+  return (
+    <div className='my-10'>
+      {title ? <h2 className='h2-bold mb-4'>{title}</h2> : null}
+      {limitedData.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((product: any) => (
+            <ProductCard key={product.name} product={product} />
+          ))}
+        </div>
+      ) : (
+        <div>
+          <p>No products found</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ProductList;
