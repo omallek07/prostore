8f3d30df98dbaefcdebc2b9b1dbcaa0ea5ae1a92
Kevin O'Malley <omallek@gmail.com>
2026-01-12 09:01:31 -0500

    Set up neon serverless

--- db/seed.ts
+++ db/seed.ts
@@ -1,18 +1,7 @@
-import { PrismaClient } from '@/lib/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config';
-
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({
-  connectionString,
-});
-
+import { prisma } from './prisma';
 import sampleData from './sample-data';
 
 async function main() {
-  const prisma = new PrismaClient({ adapter });
-
   console.log('Seeding database with sample data...');
   await prisma.product.deleteMany();
   await prisma.product.createMany({
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,21 +1,11 @@
 'use server';
 
-import { PrismaClient } from '@/lib/generated/prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config';
-
+import { prisma } from '@/db/prisma';
 import { convertToPlainObject } from '../utils';
 import { LATEST_PRODUCTS_LIMIT } from '../constants';
 
-const connectionString = `${process.env.DATABASE_URL}`;
-
-const adapter = new PrismaPg({
-  connectionString,
-});
-
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient({ adapter });
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: { createdAt: 'desc' },
