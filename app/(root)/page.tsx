454cc6e9bfcbe43e535a970593c7160e4fd39708
Kevin O'Malley <omallek@gmail.com>
2026-03-30 11:05:23 -0400

    Add carousel banner for products

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
+import ProductCarousel from '@/components/shared/product/product-carousel';
 import ProductList from '@/components/shared/product/product-list';
-import { getLatestProducts } from '@/lib/actions/product.actions';
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from '@/lib/actions/product.actions';
 import { LATEST_PRODUCTS_LIMIT } from '@/lib/constants';
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
   return (
     <>
+      {featuredProducts.length > 0 ? (
+        <ProductCarousel data={featuredProducts} />
+      ) : null}
       <ProductList
         data={latestProducts}
         title='Newest Arrivals'
