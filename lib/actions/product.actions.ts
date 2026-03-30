122469dc9ade37755ec258a51a7346da19c77114
Kevin O'Malley <omallek@gmail.com>
2026-03-30 10:50:06 -0400

    Add sidebar drawer for search functionality

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -123,3 +123,13 @@ export async function updateProduct(data: z.infer<typeof updateProductSchema>) {
     return defaultErrorRes(error);
   }
 }
+
+// Get all categories
+export async function getAllCategories() {
+  const data = await prisma.product.groupBy({
+    by: ['category'],
+    _count: true,
+  });
+
+  return data;
+}
